52d2dab7e1887bedac047fdaf40f46346a3048da
Daniel Gabinio <dangabinio@gmail.com>
2022-07-14 18:27:05 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/Notification-Button"
+
 function App() {
-  return(
-    <h1>Olá mundo</h1>
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton />
+      <NotificationButton/>
+    </>
   )
 }
 
